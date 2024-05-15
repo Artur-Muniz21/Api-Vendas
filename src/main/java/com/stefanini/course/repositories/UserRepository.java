81e3e8f450ffe8bb6d76bd093c541923f806d6a5
Artur-Muniz21 <arturvictor2107@gmail.com>
2024-05-14 21:13:09 -0300

    feat: Service e get do user por id

--- src/main/java/com/stefanini/course/repositories/UserRepository.java
+++ src/main/java/com/stefanini/course/repositories/UserRepository.java
@@ -5,7 +5,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.stefanini.course.entities.User;
 
 public interface UserRepository extends JpaRepository<User, Long>{
-
-	
-	
 }
