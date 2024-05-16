02cf405d007f84db6d0063b3d15e237cc8f5d414
Artur-Muniz21 <arturvictor2107@gmail.com>
2024-05-16 14:05:18 -0300

    feat: excção para delete com usários com pedidos

--- src/main/java/com/stefanini/course/services/UserService.java
+++ src/main/java/com/stefanini/course/services/UserService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.stefanini.course.entities.User;
 import com.stefanini.course.repositories.UserRepository;
+import com.stefanini.course.services.exceptions.DatabaseException;
 import com.stefanini.course.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -30,8 +33,23 @@ public class UserService {
 		return repository.save(obj);
 	}
 	
+	
 	public void delete(Long id) {
-		repository.deleteById(id);
+		try {
+			
+			repository.deleteById(id);
+			
+			//RuntimeException generico para erros de execução, usar para achar o nomde da exceção
+			//e.printStackTrace() mostra o tipo de exceção gerada
+		}catch(EmptyResultDataAccessException e){ 
+			
+			throw new ResourceNotFoundException(id);
+			
+		}catch(DataIntegrityViolationException  e) {
+			
+			throw new DatabaseException(e.getMessage()); // exceção da camada de serviço
+			
+		}
 	}
 	
 	public User update(Long id, User obj) {
