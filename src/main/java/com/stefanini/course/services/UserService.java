9801be424231ab552dacbe48950461a432e10d3e
Artur-Muniz21 <arturvictor2107@gmail.com>
2024-05-16 14:11:18 -0300

    feat: esceção para put com id errado

--- src/main/java/com/stefanini/course/services/UserService.java
+++ src/main/java/com/stefanini/course/services/UserService.java
@@ -13,6 +13,8 @@ import com.stefanini.course.repositories.UserRepository;
 import com.stefanini.course.services.exceptions.DatabaseException;
 import com.stefanini.course.services.exceptions.ResourceNotFoundException;
 
+import jakarta.persistence.EntityNotFoundException;
+
 @Service
 public class UserService {
 	
@@ -53,9 +55,14 @@ public class UserService {
 	}
 	
 	public User update(Long id, User obj) {
-		User entity = repository.getReferenceById(id); //deixa um obk monitorado pelo JPA para depois efetuar uma operação
-		updateData(entity, obj); // atualiza o entity baseado no obj passado
-		return repository.save(entity);
+		try {
+			User entity = repository.getReferenceById(id); //deixa um obk monitorado pelo JPA para depois efetuar uma operação
+			updateData(entity, obj); // atualiza o entity baseado no obj passado
+			return repository.save(entity);
+		}catch (EntityNotFoundException  e){
+			throw new ResourceNotFoundException(id); //erro na busca do id
+		}
+
 	}
 	
 	public void updateData(User entity, User obj) {
